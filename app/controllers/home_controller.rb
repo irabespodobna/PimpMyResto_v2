438cbc9b5805cb30711923e62d0e111a91512425
PaulineLabaisse <pauline.labaisse@orange.fr>
2018-12-03 14:23:42 +0100

    Links navbar ready

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
     def home
     end
+
+    def browse
+    end
+
+    def pimped
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get '/', to: 'home#home'
+  get '/browse', to: 'home#browse'
+  get '/pimped', to: 'home#pimped'
+  
   get '/resto', to: 'step_page#resto'
   get '/survey', to: 'step_page#survey'
   get '/foodstyle', to: 'step_page#foodstyle'
+
 end
