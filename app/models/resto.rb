a16fb536e96fef74e9f19338b6f19a1770dc8df3
irabespodobna <penkovskaya.irina@gmail.com>
2018-12-06 15:08:20 +0100

    resto db restrictions

--- app/models/resto.rb
+++ app/models/resto.rb
@@ -1,3 +1,5 @@
 class Resto < ApplicationRecord
-
+  validates :name, presence: { message: "Vous devriez mettre un nom pour le restaurant." }
+  validates :name, uniqueness: { message: "Ce nom existe déjà." }
+  validates :address, presence: { message: "Vous devriez mettre une adresse pour le restaurant." }
 end
