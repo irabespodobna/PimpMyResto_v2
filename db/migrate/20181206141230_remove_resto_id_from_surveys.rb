ca3d6b9a7af5f3111a8ab7d81b516354ea038172
irabespodobna <penkovskaya.irina@gmail.com>
2018-12-07 14:56:05 +0100

    db migration changes

--- db/migrate/20181206141230_remove_resto_id_from_surveys.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRestoIdFromSurveys < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :surveys, :resto_id, :biginit
-  end
-end
--- db/migrate/20181206141325_remove_user_id_from_surveys.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromSurveys < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :surveys, :user_id, :biginit
-  end
-end
