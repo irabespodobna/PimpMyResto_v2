f2050d403e8919b36742661d76349f38dbfd5502
irabespodobna <penkovskaya.irina@gmail.com>
2018-12-14 02:22:14 +0100

    redirection after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-      if session[:resto_id].present?
-        "/restos/#{session[:resto_id]}"
+      if current_user.present?
+        "/restos/search"
       else
         #if there is not temp list in the session proceed as normal
         super
       end
-    end
+  end
 end
