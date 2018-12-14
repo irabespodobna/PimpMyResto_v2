dfa0e75400b45f2b08ddb216b0cc74ae005ce081
PaulineLabaisse <pauline.labaisse@orange.fr>
2018-12-14 15:23:12 +0100

    changement brackground et sign out

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_200208) do
+ActiveRecord::Schema.define(version: 2018_12_13_181546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,9 @@ ActiveRecord::Schema.define(version: 2018_12_11_200208) do
     t.string "last_name"
     t.text "comment"
     t.bigint "foodstyle_id"
+    t.integer "foodstyle_category_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["foodstyle_category_id"], name: "index_users_on_foodstyle_category_id"
     t.index ["foodstyle_id"], name: "index_users_on_foodstyle_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
