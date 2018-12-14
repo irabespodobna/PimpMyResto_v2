7e2a0183a2354ded3526645b53a7374c0f5736fb
victorhebdo <victor@gabella.org>
2018-12-14 16:49:31 +0100

    css page search

--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,6 @@ ActiveRecord::Schema.define(version: 2018_12_13_181546) do
     t.string "name"
   end
 
-  create_table "foodstyles", force: :cascade do |t|
-    t.string "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "frequency_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,12 +42,6 @@ ActiveRecord::Schema.define(version: 2018_12_13_181546) do
     t.string "name"
   end
 
-  create_table "meals", force: :cascade do |t|
-    t.string "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "restos", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -63,19 +51,7 @@ ActiveRecord::Schema.define(version: 2018_12_13_181546) do
     t.index ["googleid"], name: "index_restos_on_googleid"
   end
 
-  create_table "surveymeals", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "meal_id"
-    t.index ["meal_id"], name: "index_surveymeals_on_meal_id"
-  end
-
   create_table "surveys", force: :cascade do |t|
-    t.string "budget"
-    t.string "distance"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "surveymeal_id"
     t.integer "foodstyle_category_id"
     t.integer "resto_id"
     t.integer "user_id"
@@ -87,7 +63,6 @@ ActiveRecord::Schema.define(version: 2018_12_13_181546) do
     t.index ["foodstyle_category_id"], name: "index_surveys_on_foodstyle_category_id"
     t.index ["frequency_category_id"], name: "index_surveys_on_frequency_category_id"
     t.index ["resto_id"], name: "index_surveys_on_resto_id"
-    t.index ["surveymeal_id"], name: "index_surveys_on_surveymeal_id"
     t.index ["user_id"], name: "index_surveys_on_user_id"
   end
 
@@ -102,15 +77,10 @@ ActiveRecord::Schema.define(version: 2018_12_13_181546) do
     t.string "first_name"
     t.string "last_name"
     t.text "comment"
-    t.bigint "foodstyle_id"
     t.integer "foodstyle_category_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["foodstyle_category_id"], name: "index_users_on_foodstyle_category_id"
-    t.index ["foodstyle_id"], name: "index_users_on_foodstyle_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "surveymeals", "meals"
-  add_foreign_key "surveys", "surveymeals"
-  add_foreign_key "users", "foodstyles"
 end
