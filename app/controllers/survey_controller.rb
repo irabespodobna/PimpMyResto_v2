360bad43a12040be2e370db8f036db7f74385d53
irabespodobna <penkovskaya.irina@gmail.com>
2018-12-16 18:25:48 +0100

    make sure users can only vote once

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -23,11 +23,22 @@ class SurveyController < ApplicationController
       session[:survey_data] = params
       session[:resto_id] = params[:resto_id]
       redirect_to new_user_registration_path
-    else
-      a = Survey.create foodstyle_category_id: params[:foodstyle_category_id], resto_id: params[:resto_id],  budget_category_id: params[:budget_category_id], distance_category_id: params[:distance_category_id], frequency_category_id: params[:frequency_category_id]
-      puts a.errors.inspect
-      redirect_to "/restos/#{params[:resto_id]}"
+      return
     end
+    # find surveys from the same user for the same restaurant
+    candidates = Survey.where(resto_id: params[:resto_id], user_id: current_user.id)
+    if candidates.first
+      flash[:alert] = "Vous avez déjà répondu au questionnaire pour ce restaurant!"
+      redirect_to "/restos/#{params[:resto_id]}"
+      return
+    end
+    survey = Survey.new foodstyle_category_id: params[:foodstyle_category_id], resto_id: params[:resto_id],  budget_category_id: params[:budget_category_id], distance_category_id: params[:distance_category_id], frequency_category_id: params[:frequency_category_id], user_id:  current_user.id
+    if survey.save
+      flash[:success] = "Votre participation à bien été prise en compte!"
+    else
+      flash[:alert] = "Il y a un problème, votre participation n'a pas été prise en compte"
+    end
+    redirect_to "/restos/#{params[:resto_id]}"
   end
 
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ApplicationRecord
   belongs_to :foodstyle_category
   belongs_to :resto
+  belongs_to :user
   validates :foodstyle_category_id, presence: { message: "You should choose a foodstyle." }
 end
